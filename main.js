ffb1b157cf3a32ed53779dc59d9e8e7f225312c3
Matt <nemeth.matthew@outlook.com>
2024-10-31 00:12:02 -0400

    bouncing cube

--- main.js
+++ main.js
@@ -17,10 +17,33 @@ if ( WebGL.isWebGL2Available() ) {
     camera.position.z = 5;
     scene.add( cube );
 
+    let speedLR = 0.01;
+    let speedUD = 0.02;
+    let directionLR = -1;
+    let directionUD = -1;
+    let limitLR = 2;
+    let limitUD = 2;
+
     function animate() {
         cube.rotation.x += 0.01;
         cube.rotation.y += 0.01;
 
+        // Move cube Left and Right
+        if (cube.position.x >= limitLR) {
+            directionLR = -1; // Reverse direction to left
+        } else if (cube.position.x <= -limitLR) {
+            directionLR = 1; // Reverse direction to right
+        }
+
+        // Move cube up and down
+        if (cube.position.y >= limitUD){
+            directionUD = -1; // Reverse direction down
+        } else if (cube.position.y <= -limitUD) {
+            directionUD = 1; // Reverse direction up
+        }
+
+        cube.position.x += (directionLR * speedLR);
+        cube.position.y += (directionUD * speedUD);
         renderer.render( scene, camera );
     }
 } else {
